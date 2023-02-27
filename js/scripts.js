a582a13e60f8fc2204bc26c349d42a30ee2008df
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-02-27 14:24:19 +0200

    Practice w/ arrays, loops, obj

--- js/scripts.js
+++ js/scripts.js
@@ -304,7 +304,43 @@ function isBudgetEnough(data) {
   });
   volume = square * data.height;
 
-  return  data.budget >= (volume * data.moneyPer1m3)?'Бюджета достаточно':'Бюджета недостаточно';
-
+  return data.budget >= volume * data.moneyPer1m3
+    ? "Бюджета достаточно"
+    : "Бюджета недостаточно";
 }
-isBudgetEnough(shoppingMallData)
+isBudgetEnough(shoppingMallData);
+
+//---------
+const students = [
+  "Peter",
+  "Andrew",
+  "Ann",
+  "Mark",
+  "Josh",
+  "Sandra",
+  "Cris",
+  "Bernard",
+  "Takesi",
+  "Sam",
+];
+
+function sortStudentsByGroups(arr) {
+  let team1 = [],
+    team2 = [],
+    team3 = [],
+    rest = [];
+  arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (i < 3) team1.push(arr[i]);
+    else if (i < 6) team2.push(arr[i]);
+    else if (i < 9) team3.push(arr[i]);
+    else rest.push(arr[i]);
+  }
+  return [
+    team1,
+    team2,
+    team3,
+    `Оставшиеся студенты: ${rest.length === 0 ? "-" : rest.join(", ")}`,
+  ];
+}
+sortStudentsByGroups(students);
