21c3aa6cc92486d261b9eb8af3646bc1d4f2e790
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-03-01 16:11:36 +0200

    Recursion

--- start/js/scripts.js
+++ start/js/scripts.js
@@ -346,3 +346,11 @@ function sortStudentsByGroups(arr) {
 sortStudentsByGroups(students);
 //-------
 
+function factorial(n){
+if(n >= 1)
+  return n * factorial(n - 1);
+else
+  return 1;
+}
+
+console.log(factorial(5))
