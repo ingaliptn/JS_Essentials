962e137f06cf0a3ef28d8e83550b3bb3e91bfdfa
Ingaliptnn <ipz201_tma@student.ztu.edu.ua>
2023-03-06 19:45:35 +0200

    Refactor

--- project/js/script.js
+++ project/js/script.js
@@ -19,20 +19,27 @@ const adv = document.querySelectorAll(".promo__adv img"),
   checkFilm = document.querySelector("input[type='checkbox']"),
   filmName = filmForm.querySelector("input");
 
-//adv.remove();
-adv.forEach((i) => {
-  i.remove();
-});
-resent.innerHTML = "";
+const delAdv = (arr) => {
+  arr.forEach((i) => {
+    i.remove();
+  });
+};
 
-genre.textContent = "ДРАМА";
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-function showMovie() {
-  movieDB.movies.sort();
+const makeChange = () => {
   resent.innerHTML = "";
-  movieDB.movies.forEach((film, i) => {
-    resent.innerHTML += `
+  genre.textContent = "ДРАМА";
+  poster.style.backgroundImage = 'url("img/bg.jpg")';
+};
+
+const sortArr = (arr) => {
+  arr.sort();
+};
+
+function showMovie(parent, film) {
+  parent.innerHTML = "";
+  sortArr(movieDB.movies);
+  film.movies.forEach((film, i) => {
+    parent.innerHTML += `
     <li class="promo__interactive-item">${++i}. ${film}
                             <div class="delete"></div>
                         </li>
@@ -43,22 +50,28 @@ function showMovie() {
     btn.addEventListener("click", () => {
       btn.parentElement.remove();
       movieDB.movies.splice(i, 1);
-      showMovie();
+      showMovie(parent, film);
     });
   });
 }
-showMovie();
+const addFilmToArr = () => {
+  addFilm.addEventListener("click", (e) => {
+    e.preventDefault();
+    let fName = filmName.value;
+    if (fName.length > 21) {
+      fName = fName.slice(0, 21);
+      fName += "...";
+    }
+    if (checkFilm.checked == true) {
+      console.log("Добавляем любимый фильм");
+    }
+    movieDB.movies.push(fName);
+    showMovie(resent, movieDB);
+  });
+};
 
-addFilm.addEventListener("click", (e) => {
-  e.preventDefault();
-  let fName = filmName.value;
-  if (fName.length > 21) {
-    fName = fName.slice(0, 21);
-    fName += "...";
-  }
-  if (checkFilm.checked == true) {
-    console.log("Добавляем любимый фильм");
-  }
-  movieDB.movies.push(fName);
-  showMovie();
-});
+delAdv(adv);
+makeChange();
+sortArr(movieDB.movies);
+showMovie(resent, movieDB);
+addFilmToArr()
