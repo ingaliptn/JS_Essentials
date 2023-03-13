dd01a4ba2eb9f83956212412a67a1a9c002aa664
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-03-13 12:33:52 +0200

    Refactor timer

--- Food_dist/js/index.js
+++ Food_dist/js/index.js
@@ -35,14 +35,23 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const deadLine = "2023-3-9";
+  const deadLine = "2023-5-20";
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-      minutes = Math.floor((t / 1000 / 60) % 60),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((t / 1000 / 60) % 60);
       seconds = Math.floor((t / 1000) % 60);
+    }
 
     return {
       total: t,
@@ -53,11 +62,11 @@ window.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  function getZero(num){
-    if(num >= 0 && num < 10){
-        return `0${num}`;
-    }else{
-        return num;
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
     }
   }
 
