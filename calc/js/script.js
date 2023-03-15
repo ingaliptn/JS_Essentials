7c03ca0c6f29b65cb48d4b000c2395b4c9f48952
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-03-15 13:38:35 +0200

    Work w/ AJAX

---
+++ calc/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const inputUAH = document.querySelector("#uah"),
+  inputUSD = document.querySelector("#usd");
+
+inputUAH.addEventListener("input", () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
+
+  request.addEventListener("load", () => {
+    if (request.status == 200) {
+      const data = JSON.parse(request.response);
+      inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+    } else {
+        inputUSD.value = "error"
+    }
+  });
+});
